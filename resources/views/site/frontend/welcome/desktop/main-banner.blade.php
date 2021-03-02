e837e76bc34de1d19121cc4d267b5bfc1cb1664e
lislmaka <70829116+lislmaka@users.noreply.github.com>
2021-03-02 11:22:51 +0300

    change main banner

--- resources/views/site/frontend/welcome/desktop/main-banner.blade.php
+++ resources/views/site/frontend/welcome/desktop/main-banner.blade.php
@@ -4,12 +4,12 @@
 
         <div class="row">
             <div class="col-8">
-                <h1 class="display-3 fw-bold text-white">
+                <h1 class="display-3 fw-bold text-dark">
                     @lang('Кейтеринг, банкеты и фуршеты')
                 </h1>
-                <p class="h2 my-3 text-white">
+                <p class="h2 my-3 fw-bold text-dark">
                     @lang('Укажите тип и формат вашего мероприятия и мы составим для вас')
-                    <span class="badge bg-light text-dark rounded-pill">готовое меню в течении 1 часа</span>
+                    <span class="badge bg-light text-dark rounded-pill">готовое меню в течении 1 часа &#128077;</span>
                 </p>
             </div>
             <div class="col-4">
